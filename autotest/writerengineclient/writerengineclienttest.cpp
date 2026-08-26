a8adc13e6d0e33ba50e228ea382aa6b623119552
Oracle <anoracleofra@gmail.com>
2026-08-26 05:51:14 -0600

    Add multi-frame sidecar parser regression test

--- autotest/writerengineclient/writerengineclienttest.cpp
+++ autotest/writerengineclient/writerengineclienttest.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#include <QJsonDocument>
 #include <QMetaObject>
 #include <QProcess>
 #include <QSignalSpy>
@@ -14,6 +15,19 @@
 
 using namespace ghostwriter;
 
+namespace
+{
+QByteArray responseFrame(const QString &requestId, const QJsonObject &result = {})
+{
+    QJsonObject response;
+    response.insert(QStringLiteral("request_id"), requestId);
+    response.insert(QStringLiteral("ok"), true);
+    response.insert(QStringLiteral("result"), result);
+    const QByteArray body = QJsonDocument(response).toJson(QJsonDocument::Compact);
+    return QByteArrayLiteral("Content-Length: ") + QByteArray::number(body.size()) + QByteArrayLiteral("\r\n\r\n") + body;
+}
+}
+
 class WriterEngineClientTest : public QObject
 {
     Q_OBJECT
@@ -23,6 +37,7 @@ private slots:
     void processExitInvalidatesOutstandingRequestState();
     void automaticPerformancePolicyIsBoundedAndCpuOnly();
     void capabilitiesAreEmptyBeforeNegotiation();
+    void parsesConsecutiveFramesWithoutLosingResponses();
 };
 
 void WriterEngineClientTest::stderrIsNotReportedAsAProtocolFailure()
@@ -61,6 +76,29 @@ void WriterEngineClientTest::capabilitiesAreEmptyBeforeNegotiation()
     WriterEngineClient client;
     QCOMPARE(client.negotiatedProtocolMinor(), 0);
     QVERIFY(!client.supportsOperation(QStringLiteral("open_document")));
+}
+
+void WriterEngineClientTest::parsesConsecutiveFramesWithoutLosingResponses()
+{
+    WriterEngineClient client;
+    QSignalSpy responseSpy(&client, &WriterEngineClient::responseReceived);
+    QSignalSpy errorSpy(&client, &WriterEngineClient::engineError);
+
+    client.m_pendingOperations.insert(QStringLiteral("first"), QStringLiteral("list_profiles"));
+    client.m_pendingOperations.insert(QStringLiteral("second"), QStringLiteral("open_document"));
+    client.m_pendingOperations.insert(QStringLiteral("third"), QStringLiteral("analyze_document"));
+    client.m_buffer = responseFrame(QStringLiteral("first"))
+        + responseFrame(QStringLiteral("second"))
+        + responseFrame(QStringLiteral("third"));
+
+    client.parseMessages();
+
+    QTRY_COMPARE_WITH_TIMEOUT(responseSpy.count(), 3, 2000);
+    QCOMPARE(errorSpy.count(), 0);
+    QCOMPARE(responseSpy.at(0).at(0).toString(), QStringLiteral("first"));
+    QCOMPARE(responseSpy.at(1).at(0).toString(), QStringLiteral("second"));
+    QCOMPARE(responseSpy.at(2).at(0).toString(), QStringLiteral("third"));
+    QCOMPARE(client.m_bufferOffset, client.m_buffer.size());
 }
 
 QTEST_MAIN(WriterEngineClientTest)
